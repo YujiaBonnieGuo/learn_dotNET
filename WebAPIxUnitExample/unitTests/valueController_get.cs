088396856db66f4d357252056c00db051f909755
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2019-05-23 15:40:28 -0400

    dotnet

---
+++ WebAPIxUnitExample/unitTests/valueController_get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+
+namespace unitTests
+{
+    public class valueController_Get
+    {
+        [Fact]
+        public void shouldReturn2Values()
+        {
+            var controller = new valueController();
+            var result = controller.Get();
+            Assert.Equal(2, result.Count());
+
+        }
+    }
+}
