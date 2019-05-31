07fb97fdec0bed73e6d9ab4f4fc46aed64c2fe4c
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2019-05-31 14:03:26 -0400

    mOdeToFood exercise copy

---
+++ OdeToFood - 副本/OdeToFood/Pages/Edit.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFod.Data;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+
+        public EditModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
---
+++ OdeToFood/OdeToFod.Data/IRestaurantData.cs
@@ -0,0 +1,41 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFod.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
+        Restaurant GetById(int id);
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+        
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name="Scotis Pizza", Location= "Maryland",Cuisine=CuisineType.Mexican},
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Indian }
+            };
+        }
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantByName(string name=null)
+        {
+            return from r in restaurants
+                   where String.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
